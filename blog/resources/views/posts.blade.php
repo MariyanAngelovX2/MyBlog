b9fc192c2dddcc7049276e3349c0adcd4a5ea127
Mariyan Angelov <mariyanbgn@gmail.com>
2024-03-16 13:07:52 +0200

    Creating three posts on the homepage.

---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
+        </p>
+    </article>
+</body>
