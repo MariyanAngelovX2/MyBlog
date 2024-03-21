12d4fdf135e16f3f2a968050d86083ad778965d5
Mariyan Angelov <mariyanbgn@gmail.com>
2024-03-21 15:35:54 +0200

    Use the filesystem class to read a directory

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,27 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+    <?php foreach($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
-        </p>
+        <?= $post; ?>
     </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt est facere vitae, quaerat nulla minus eos officia consequatur! Sunt maiores repellat qui quaerat soluta cum, pariatur veniam nemo? Assumenda.
-        </p>
-    </article>
+    <?php endforeach; ?>
 </body>
