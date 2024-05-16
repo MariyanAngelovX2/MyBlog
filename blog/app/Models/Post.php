cdfa100da0424f7005555f8405116ef90dfc3adc
Mariyan Angelov <mariyanbgn@gmail.com>
2024-05-16 12:46:57 +0300

    Database seeding

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -16,5 +17,10 @@ class Post extends Model
         //hasOne, hasMany, belongsTo, belongsToMany
         //belongsTo
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->BelongsTo(User::class);
     }
 }
